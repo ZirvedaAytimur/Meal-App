9a1c42785e28e543877547bb86b840cd8200e57b
ZirvedaAytimur <zirvedaytimur@gmail.com>
2019-08-29 15:36:57 +0300

    Redux data and navigation options

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, Text, StyleSheet, ScrollView, Image } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import { useSelector } from 'react-redux';
@@ -20,6 +20,12 @@ const MealDetailScreen = props => {
   const mealId = props.navigation.getParam('mealId');
 
   const selectedMeal = availableMeals.find(meal => meal.id === mealId);
+
+  useEffect(() => {
+    props.navigation.setParams({ mealTitle: selectedMeal.title });
+  },
+    [selectedMeal]
+  );
 
   return (
     <ScrollView>
@@ -43,9 +49,13 @@ const MealDetailScreen = props => {
 
 MealDetailScreen.navigationOptions = navigationData => {
   const mealId = navigationData.navigation.getParam('mealId');
-  const selectedMeal = MEALS.find(meal => meal.id === mealId);
+
+  const mealTitle = navigationData.navigation.getParam('mealTitle');
+
+  // const selectedMeal = MEALS.find(meal => meal.id === mealId);
+
   return {
-    headerTitle: selectedMeal.title,
+    headerTitle: mealTitle,
     headerRight:
       <HeaderButtons HeaderButtonComponent={HeaderButton} >
         <Item
